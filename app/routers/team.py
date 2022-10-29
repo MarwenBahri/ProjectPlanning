e5df824de28594847d3b685d49d94737bec0a2ae
mahdi cheikhrouhou <cheikhrouhoumahdi@gmail.com>
2022-10-29 22:27:00 +0200

    fixed some bugs with the team routers

--- app/routers/team.py
+++ app/routers/team.py
@@ -1,3 +1,4 @@
+from typing import List
 from fastapi import APIRouter, Depends, HTTPException, Response, status
 from sqlalchemy.orm import Session
 
@@ -5,10 +6,19 @@ from .. import models, schemas
 from ..database import get_db
 
 router = APIRouter(
-    prefix="teams",
+    prefix="/teams",
     tags=["Team"]
 )
 
+
+@router.get("/",  response_model=List[schemas.TeamOut])
+def get_teams(db: Session = Depends(get_db)):
+    user_id = 1
+    teams = db.query(models.Team
+                    ).join(models.UserTeam, models.Team.id == models.UserTeam.team_id
+                           ).filter(models.UserTeam.user_id == user_id,
+                                    ).all()
+    return teams
 
 @router.get("/{team_id}/",  response_model=schemas.TeamOut)
 def get_team(team_id: int, db: Session = Depends(get_db)):
@@ -27,10 +37,13 @@ def get_team(team_id: int, db: Session = Depends(get_db)):
 @router.post("/",  status_code=status.HTTP_201_CREATED, response_model=schemas.TeamOut)
 def create_team(team: schemas.TeamBase, db: Session = Depends(get_db)):
     user_id = 1
-    new_team = models.Project(**team.dict(), owner_id=user_id)
+    new_team = models.Team(**team.dict(), owner_id=user_id)
     db.add(new_team)
     db.commit()
     db.refresh(new_team)
+    user_team = models.UserTeam(user_id=user_id, team_id=new_team.id)
+    db.add(user_team)
+    db.commit()
     return new_team
 
 
