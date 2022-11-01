5f843e011fd5f10097e0cc0a78b778c20771777c
MarwenBahri <71530215+MarwenBahri@users.noreply.github.com>
2022-11-01 22:26:11 +0100

    removed explicit settings

--- app/routers/pass_reset.py
+++ app/routers/pass_reset.py
@@ -11,19 +11,19 @@ from jinja2 import Environment, select_autoescape, PackageLoader
 
 from ..database import get_db
 from .. import models, utils, schemas
-
+from ..config import settings
 
 class EmailSchema(BaseModel):
     email: EmailStr
 
 
 conf = ConnectionConfig(
-    MAIL_USERNAME = "mailing.fishpals@gmail.com",
-    MAIL_PASSWORD = "zwgccsqrvfenznge",
-    MAIL_FROM = "mailing.fishpals@gmail.com",
-    MAIL_PORT = 587,
-    MAIL_SERVER = "smtp.gmail.com",
-    MAIL_FROM_NAME="Project Planning",
+    MAIL_USERNAME = settings.mail_username,
+    MAIL_PASSWORD = settings.mail_password,
+    MAIL_FROM = settings.mail_from,
+    MAIL_PORT = settings.mail_port,
+    MAIL_SERVER = settings.mail_server,
+    MAIL_FROM_NAME= settings.mail_from_name,
     MAIL_STARTTLS = True,
     MAIL_SSL_TLS = False,
     USE_CREDENTIALS = True,
